73b23c3d02e6c7916126b6a7e77109d73ceb6f5a
Maurice1995 <maurice@riddleandcode.com>
2022-12-25 00:04:56 +0100

    Christmas_test

---
+++ script.js
@@ -0,0 +1,26 @@
+function changeImage1(x, image) {
+    if (x == 1) {
+        image.src = "TG-C-shadow.png";
+    }
+    if (x == 2) {
+        image.src = "TG-C.PNG";
+    }
+}
+
+function changeImage2(x, image) {
+    if (x == 1) {
+        image.src = "TG-E-shadow.png";
+    }
+    if (x == 2) {
+        image.src = "TG-E.PNG";
+    }
+}
+
+function changeImage3(x, image) {
+    if (x == 1) {
+        image.src = "TG-I-shadow.png";
+    }
+    if (x == 2) {
+        image.src = "TG-I.PNG";
+    }
+}
