f0dcaab184280327b3f7097e58b25d7f1b61beb1
Maurice Pannard <maurice.pannard@gmail.com>
2024-04-26 11:57:08 +0200

    Update script.js
    png to PNG

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function changeImage1(x, image) {
     if (x == 1) {
         //image.src = "TG-C-shadow.png";
-        image.src = "TG-C.png";
+        image.src = "TG-C.PNG";
     }
     if (x == 2) {
         image.src = "TG-C.PNG";
@@ -11,7 +11,7 @@ function changeImage1(x, image) {
 function changeImage2(x, image) {
     if (x == 1) {
         //image.src = "TG-E-shadow.png";
-        image.src = "TG-E.png";
+        image.src = "TG-E.PNG";
     }
     if (x == 2) {
         image.src = "TG-E.PNG";
@@ -21,7 +21,7 @@ function changeImage2(x, image) {
 function changeImage3(x, image) {
     if (x == 1) {
         //image.src = "TG-I-shadow.png";
-        image.src = "TG-I.png";
+        image.src = "TG-I.PNG";
     }
     if (x == 2) {
         image.src = "TG-I.PNG";
