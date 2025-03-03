bf87b2deba6a80d401dde2f7903b6f73fab78e17
Maurice Pannard <maurice.pannard@gmail.com>
2025-03-03 17:33:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-function changeImage1(x, image) {
-    if (x == 1) {
-        //image.src = "TG-C-shadow.png";
-        image.src = "TG-C.PNG";
-    }
-    if (x == 2) {
-        image.src = "TG-C.PNG";
-    }
-}
+// function changeImage1(x, image) {
+//     if (x == 1) {
+//         //image.src = "TG-C-shadow.png";
+//         image.src = "TG-C.PNG";
+//     }
+//     if (x == 2) {
+//         image.src = "TG-C.PNG";
+//     }
+// }
 
-function changeImage2(x, image) {
-    if (x == 1) {
-        //image.src = "TG-E-shadow.png";
-        image.src = "TG-E.PNG";
-    }
-    if (x == 2) {
-        image.src = "TG-E.PNG";
-    }
-}
+// function changeImage2(x, image) {
+//     if (x == 1) {
+//         //image.src = "TG-E-shadow.png";
+//         image.src = "TG-E.PNG";
+//     }
+//     if (x == 2) {
+//         image.src = "TG-E.PNG";
+//     }
+// }
 
-function changeImage3(x, image) {
-    if (x == 1) {
-        //image.src = "TG-I-shadow.png";
-        image.src = "TG-I.PNG";
-    }
-    if (x == 2) {
-        image.src = "TG-I.PNG";
-    }
-}
+// function changeImage3(x, image) {
+//     if (x == 1) {
+//         //image.src = "TG-I-shadow.png";
+//         image.src = "TG-I.PNG";
+//     }
+//     if (x == 2) {
+//         image.src = "TG-I.PNG";
+//     }
+// }
 
